9d6d758ea80c9314e08683094cc724f36ed38100
Ivan Levchenko <donnie.il.drummer@gmail.com>
2016-06-03 14:53:11 +0300

    Init

--- src/TelegramBot.php
+++ src/TelegramBot.php
@@ -8,26 +8,48 @@
 
 namespace Donnie\TelegramBot;
 
+use Donnie\TelegramBot\Entities\Conversation;
 use GuzzleHttp\Client;
 use Config;
 
+/**
+ * Class TelegramBot.
+ * 
+ * @package Donnie\TelegramBot.
+ */
 class TelegramBot
 {
-    protected $api;
+    /**
+     * @var Client
+     */
     protected $client;
-    
+
+    /**
+     * TelegramBot constructor.
+     */
     public function __construct()
     {
         $this->client = new Client();
         $this->api_url = Config::get('telegramBot.api_url') . '/bot' . Config::get('telegramBot.bot_token') . '/';
     }
 
+    /**
+     *
+     */
     public function initWebhook()
     {
         echo $this->api_url . 'setWebhook?url=' . Config::get('telegramBot.webhook_url');
         $this->client->request('GET', $this->api_url . 'setWebhook?url=' . Config::get('telegramBot.webhook_url'));
     }
 
+    /**
+     * Send reply.
+     * 
+     * @param Conversation $conversation.
+     * @param string|Illuminate\View\View $message.
+     * 
+     * @return void.
+     */
     public function reply($conversation, $message)
     {
         $response = $this->client->request('POST', $this->api_url . 'sendMessage', [
